7382044e8f2e01ba597b74b82bdf6003c41ba1ae
dae <notloneth@gmail.com>
2022-08-23 12:49:14 +0700

    yes

--- bot.js
+++ bot.js
@@ -1,19 +1,9 @@
-/*
- * Discord Bot Zueira The Maid
- * Version 0.9
- * Zueira test 2
- */
-
 const Files = require(require('path').join(__dirname, 'js', 'Main.js')).Files;
 
 if(!process.send) {
-
-Files.initStandalone();
-
+	Files.initStandalone();
 } else {
-
-process.on('message', function(content) {
-	Files.initBotTest(content);
-});
-
+	process.on('message', function(content) {
+		Files.initBotTest(content);
+	});
 }
